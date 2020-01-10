4df236899cfd6118124668f78e547bb6c3787f41
HBhakunamatata <hanbo9501@gmail.com>
2020-01-10 15:54:45 +0800

    note book for interface version 1

---
+++ src/Ch08/Exercise/exercise05/SolonSword.java
@@ -0,0 +1,14 @@
+package Ch08.Exercise.exercise05;
+
+/*
+ *Exercise05: Create an interface containing three methods, in its own package.
+ * Implement the interface in a different package.
+*
+* Exercise06: Prove that all the methods in an interface are automatically public.
+ */
+
+public interface SolonSword {
+    void hitWithMouth();
+    void hitwithleft();
+    void hitWithRight();
+}
---
+++ src/Ch08/Exercise/exercise3/BasedPrint.java
@@ -0,0 +1,8 @@
+package Ch08.Exercise.exercise3;
+
+abstract public class BasedPrint {
+    BasedPrint(){
+        print();
+    }
+    abstract public void print ();
+}
