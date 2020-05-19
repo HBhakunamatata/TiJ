75ec0fabc55306c2818f4b3ec2bf319dfeb39c5e
HBhakunamatata <hanbo9501@gmail.com>
2020-05-19 21:43:24 +0800

    finish Ch09

--- src/Ch09/Exercises/InheritInner.java
+++ src/Ch09/Exercises/InheritInner.java
@@ -1,5 +1,12 @@
 package Ch09.Exercises;
 
+/*
+Exercise 26: (2) Create a class with an inner class that has a non-default constructor
+(one that takes arguments). Create a second class with an inner class that inherits from the
+first inner class.
+
+ */
+
 class WithInner {
     class Inner {
 
