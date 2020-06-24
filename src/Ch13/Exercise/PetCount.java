a723160ca4d5f9861cb3450d0274f2e1b8ff84f0
HBhakunamatata <hanbo9501@gmail.com>
2020-06-24 23:40:17 +0800

    keep on Ch13 (P.432)

---
+++ src/Ch13/Exercise/PetCount.java
@@ -0,0 +1,31 @@
+package Ch13.Exercise;
+
+// Using instanceof.
+import Ch13.Exercise.pets.*;
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+public class PetCount {
+    static class PetCounter extends HashMap<String, Integer> {
+        public void count(String type) {
+            Integer quantity = get(type);
+            if (quantity == null) {
+                put(type, 1);
+            } else {
+                put(type, quantity + 1);
+            }
+        }
+    }
+
+    public static void countPet(PetCreator petCreator) {
+        PetCounter petCounter = new PetCounter();
+        for (Pet pet: petCreator.createArray(10)) {
+            petCounter.count( pet.getClass().getSimpleName() );
+        }
+        print(petCounter);
+    }
+
+    public static void main(String[] args) {
+        countPet(new ForNameCreator());
+    }
+}
